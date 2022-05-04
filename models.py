6e65addad861c0297381d95c7d677695aea5d7d5
danalmeidap <dbatistadealmeida23@gmail.com>
2022-05-04 09:45:08 -0300

    ToDo model created

---
+++ models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import Column, String, UniqueConstraint
+from sqlmodel import Field, SQLModel
+
+
+class ToDo(
+    SQLModel,
+    table=True,
+    __table_args__=(UniqueConstraint("name")),
+):
+    id: Optional[int] = Field(primary_key=True, default=None, index=True)
+    name: str = Field(sa_column=Column("name", String, unique=True))
+    description: str
+    added_on: datetime = Field(default_factory=datetime.now)
+    is_active: Optional[bool] = True
